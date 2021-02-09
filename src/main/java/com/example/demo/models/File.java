58a5998b34313c49f5cad14ff27b79f2ce9086ff
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2021-02-09 17:02:44 +0000

    created models

---
+++ src/main/java/com/example/demo/models/File.java
@@ -0,0 +1,78 @@
+package com.example.demo.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "files")
+public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String ext;
+
+    @Column
+    private float size;
+
+    @ManyToOne
+    @JoinColumn(name = "folder_id", nullable = false)
+    @JsonIgnoreProperties("files")
+    private Folder folder;
+
+    public File(String name, String ext, float size, Folder folder) {
+        this.name = name;
+        this.ext = ext;
+        this.size = size;
+        this.folder = folder;
+    }
+
+    public File() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExt() {
+        return ext;
+    }
+
+    public void setExt(String ext) {
+        this.ext = ext;
+    }
+
+    public float getSize() {
+        return size;
+    }
+
+    public void setSize(float size) {
+        this.size = size;
+    }
+
+    public Folder getFolder() {
+        return folder;
+    }
+
+    public void setFolder(Folder folder) {
+        this.folder = folder;
+    }
+}
